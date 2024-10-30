e8c86dd8ca366c8207f8a081acdbbce176c9de0e
Ogaraga <raymond.ogaraga@gmail.com>
2024-10-30 23:27:11 +0100

    landingpage

--- src/component/landingpage/LandingPage.jsx
+++ src/component/landingpage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import styles from './LandingPage.module.css'
+import styles from "./LandingPage.module.css";
 import Avater from "../assets/socialnetworking.png";
 import Avater2 from "../assets/woman.png";
 import Avater4 from "../assets/chef.png";
@@ -18,14 +18,14 @@ import image8 from "../assets/eight.png";
 import image9 from "../assets/nine.png";
 import image10 from "../assets/ten.png";
 import imageOf3 from "../assets/afrocentric_bg.png";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 import Header from "../header/Header";
 import avatabtn from "../assets/members (1).png";
 import Swiper from "../swiper/Swiper";
 import LandingFooter from "../landingfooter/LandingFooter";
 
-function LandingPage() {  
+function LandingPage() {
   const navigate = useNavigate();
 
   //sign in to social networking room onclick
@@ -38,260 +38,261 @@ function LandingPage() {
     navigate("/joinnow");
   };
 
-  
   return (
     <>
-    
       <Header />
       <main className={styles.mains}>
-      <div className={styles.celebrating}>
-        <h4>
-          Celebrating Diversity,{" "}
-          <b style={{ color: "green" }}>Connecting Communities, </b>Enriching
-          lives
-        </h4>
-        <p className={styles.celep}>
-          Join us in building dynamic social networks that honor your heritage
-          and foster meaningful connections. Discover a space where you can
-          truly be yourself and embrace your community.
-        </p>
-        <Link to="/joinnow"  className={styles.btnjoin}>
-          <button type="button" className={styles.Mbtn} >Join Community Now</button>
-        </Link>
-        <span className={styles.images}>
-          <img src={image1} alt=" image" />
-          <img src={image2} alt="image" />
-          <img src={image3} alt="image" />
-          <img src={image4} alt="" />
-          <img src={image5} alt="image" />
-          <img src={image6} alt="image" />
-          <img src={image7} alt="image" />
-          <img src={image8} alt="image" />
-          <img src={image9} alt="image" />
-          <img src={image10} alt="image" />
-        </span>
-      </div>
-      <div className={styles.communities}>
-        <h1>Communities</h1>
-        <p>Join communities and conversations that fits your interests.</p>
-      </div>
-      <h1 className={styles.h1popular}>Popular Communities you can join.</h1>
-      <div className={styles.popular}>
-        <span className={styles.card1}>
-          <FaStar className={styles.star} />
-          <h2>African American Community</h2>
-          <p>
-            A community of people of African descent from all around the world.
+        <div className={styles.celebrating}>
+          <h4>
+            Celebrating Diversity,{" "}
+            <b style={{ color: "green" }}>Connecting Communities, </b>Enriching
+            lives
+          </h4>
+          <p className={styles.celep}>
+            Join us in building dynamic social networks that honor your heritage
+            and foster meaningful connections. Discover a space where you can
+            truly be yourself and embrace your community.
           </p>
-          <div className={styles.avaterbtn}>
-            <img src={avatabtn} alt="Image" />
-            <button className={styles.cardbtn}>+8M</button>
-          </div>
-        </span>
-        <span className={styles.card2}>
-          <FaStar className={styles.star} />
-          <h2>Filipino Diaspora Community</h2>
-          <p>
-            A group of people of Filipino descent from all arround the world
-          </p>
-          <div className={styles.avaterbtn}>
-            <img src={avatabtn} alt="image" />
-            <button className={styles.cardbtn}>+6M</button>
-          </div>
-        </span>
-        <div className={styles.card3}>
-          <FaStar className={styles.star} />
-          <h2>Jamaican Diaspora Community</h2>
-          <p>We talk only football here, and it’s mostly banter.</p>
-          <span className={styles.avaterbtns}>
-            <img src={avatabtn} alt="image" />
-            <button className={styles.cardbtn}>+700.2k</button>
+          <Link to="/joinnow" className={styles.btnjoin}>
+            <button type="button" className={styles.Mbtn}>
+              Join Community Now
+            </button>
+          </Link>
+          <span className={styles.images}>
+            <img src={image1} alt=" image" />
+            <img src={image2} alt="image" />
+            <img src={image3} alt="image" />
+            <img src={image4} alt="" />
+            <img src={image5} alt="image" />
+            <img src={image6} alt="image" />
+            <img src={image7} alt="image" />
+            <img src={image8} alt="image" />
+            <img src={image9} alt="image" />
+            <img src={image10} alt="image" />
           </span>
         </div>
-      </div>
-      <div className={styles.explore}>
-        <h2 onClick={handleExplore}>Explore other communities</h2>
-      </div>
-      <div className={styles.deeply}>
-        <span className={styles.deepSpan}>deeply rooted</span>
-        <h1>Embrace a true cultural experience</h1>
-        <p>
-          Explore our wide range of products, and get solutions to every of your
-          needs
-        </p>
-      </div>
-      <div className={styles.socialContainer}>
-        <div className={styles.social} onClick={handleSocialSignin}>
-          <div className={styles.imag}>
-            <img
-              src={Avater}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Social Networking</h4>
-            <p className={styles.psocial}>
-              Stay connected with family and friends. Start a group and join a
-              conversation.
+        <div className={styles.communities}>
+          <h1>Communities</h1>
+          <p>Join communities and conversations that fits your interests.</p>
+        </div>
+        <h1 className={styles.h1popular}>Popular Communities you can join.</h1>
+        <div className={styles.popular}>
+          <div className={styles.card1}>
+            <FaStar className={styles.star} />
+            <h2>African American Community</h2>
+            <p>
+              A community of people of African descent from all around the
+              world.
             </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
+            <span className={styles.avaterbtn}>
+              <img src={avatabtn} alt="Image" />
+              <button className={styles.cardbtn}>+8M</button>
+            </span>
           </div>
-        </div>
-        <div className={styles.social2}>
-          <div className={styles.imag}>
-            <img
-              src={Avater2}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Travel and Tourism</h4>
-            <p className={styles.psocial}>
-              Immerse yourself in Afrocentric destinations.
+          <div className={styles.card2}>
+            <FaStar className={styles.star} />
+            <h2>Filipino Diaspora Community</h2>
+            <p>
+              A group of people of Filipino descent from all arround the world
             </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
-        </div>
-        
-        </div>
-        <div className={styles.social4}>
-          <div className={styles.imag}>
-            <img
-              src={Avater4}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Food and Restaurant</h4>
-            <p className={styles.psocial}>
-              Indulge in a culinary journey that celebrates the Afrocentric
-              cuisine.
-            </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
+            <span className={styles.avaterbtn}>
+              <img src={avatabtn} alt="image" />
+              <button className={styles.cardbtn}>+6M</button>
+            </span>
           </div>
-        </div>
-        <div className={styles.social5}>
-          <div className={styles.imag}>
-            <img
-              src={Avater5}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Shopping</h4>
-            <p className={styles.psocial}>
-              Explore Afrocentric craftsmanship through our shopping
-              experiences.
-            </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
+          <div className={styles.card3}>
+            <FaStar className={styles.star} />
+            <h2>Jamaican Diaspora Community</h2>
+            <p>We talk only football here, and it’s mostly banter.</p>
+            <span className={styles.avaterbtns}>
+              <img src={avatabtn} alt="image" />
+              <button className={styles.cardbtn}>+700.2k</button>
+            </span>
           </div>
-        </div>
-        <div className={styles.social6}>
-          <div className={styles.imag}>
-            <img
-              src={Avater6}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Money</h4>
-            <p className={styles.psocial}>
-              Perform monetary transactions with ease and utmost security.
-            </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
+          <div/>
+          <div className={styles.explore}>
+            <h2 onClick={handleExplore}>Explore other communities</h2>
           </div>
-        </div>
-        <div className={styles.social7}>
-          <div className={styles.imag}>
-            <img
-              src={Avater7}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Tickets and Events</h4>
-            <p className={styles.psocial}>
-              Stay connected and engaged with our vibrant lineup of events and
-              concerts
+          <div className={styles.deeply}>
+            <span className={styles.deepSpan}>deeply rooted</span>
+            <h1>Embrace a true cultural experience</h1>
+            <p>
+              Explore our wide range of products, and get solutions to every of
+              your needs
             </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
           </div>
-        </div>
-        <div className={styles.social8}>
-          <div className={styles.imag}>
-            <img
-              src={Avater8}
-              alt="chat-Image/Avater"
-              className={styles.prod_image}
-            />
-            <h4 className={styles.h4social}>Videos</h4>
-            <p className={styles.psocial}>
-              Indulge in a culinary journey that celebrates the Afrocentric
-              cuisine.
-            </p>
-            <span className={styles.span}>View More </span>
-            <img
-              src={ArrowRightUp}
-              alt="AroowupRight"
-              className={styles.arrowUp}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.oneStopPlusImage}>
-        <div className={styles.oneStop}>
-          <h1>
-            A one-stop community for your{" "}
-            <b style={{ color: "green" }}>authentic cultural</b> <br />
-            lifestyle
-          </h1>
-          <p>
-            Building Communities.{" "}
-            <b style={{ color: "green" }}>Connecting People.</b>{" "}
-          </p>
-          <button className={styles.oneStopBtn}>
-            <Link to="/joinnow">Become a member</Link>
-          </button>
-        </div>
-        <div className={styles.image3}>
-          <img src={imageOf3} alt="Three persons" />
-        </div>
-      </div>
-      <div className={styles.paginationHeading}>
-        <h1>
-          Why members love <br />
-          <b>Fatherland</b>
-        </h1>
-      </div>
-      <Swiper/>
-      </main>
-      
-      <div className={styles.landFoot}>
+          <div className={styles.socialContainer}>
+            <div className={styles.social} onClick={handleSocialSignin}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Social Networking</h4>
+                <p className={styles.psocial}>
+                  Stay connected with family and friends. Start a group and join
+                  a conversation.
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
 
-      <LandingFooter/>
+            <div className={styles.social2}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater2}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Travel and Tourism</h4>
+                <p className={styles.psocial}>
+                  Immerse yourself in Afrocentric destinations.
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
+            <div className={styles.social4}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater4}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Food and Restaurant</h4>
+                <p className={styles.psocial}>
+                  Indulge in a culinary journey that celebrates the Afrocentric
+                  cuisine.
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
+            <div className={styles.social5}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater5}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Shopping</h4>
+                <p className={styles.psocial}>
+                  Explore Afrocentric craftsmanship through our shopping
+                  experiences.
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
+            <div className={styles.social6}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater6}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Money</h4>
+                <p className={styles.psocial}>
+                  Perform monetary transactions with ease and utmost security.
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
+            <div className={styles.social7}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater7}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Tickets and Events</h4>
+                <p className={styles.psocial}>
+                  Stay connected and engaged with our vibrant lineup of events
+                  and concerts
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
+            <div className={styles.social8}>
+              <div className={styles.imag}>
+                <img
+                  src={Avater8}
+                  alt="chat-Image/Avater"
+                  className={styles.prod_image}
+                />
+                <h4 className={styles.h4social}>Videos</h4>
+                <p className={styles.psocial}>
+                  Indulge in a culinary journey that celebrates the Afrocentric
+                  cuisine.
+                </p>
+                <span className={styles.span}>View More </span>
+                <img
+                  src={ArrowRightUp}
+                  alt="AroowupRight"
+                  className={styles.arrowUp}
+                />
+              </div>
+            </div>
+        </div>
+          <div className={styles.oneStopPlusImage}>
+            <div className={styles.oneStop}>
+              <h1>
+                A one-stop community for your{" "}
+                <b style={{ color: "green" }}>authentic cultural</b> <br />
+                lifestyle
+              </h1>
+              <p>
+                Building Communities.{" "}
+                <b style={{ color: "green" }}>Connecting People.</b>{" "}
+              </p>
+              <button className={styles.oneStopBtn}>
+                <Link to="/joinnow">Become a member</Link>
+              </button>
+            </div>
+            </div>
+            <div className={styles.image3}>
+              <img src={imageOf3} alt="Three persons" />
+            </div>
+          </div>        
+        <div className={styles.paginationHeading}>
+          <h1>
+            Why members love <br />
+            <b>Fatherland</b>
+          </h1>
+        </div>
+        <Swiper />
+      </main>
+
+      <div className={styles.landFoot}>
+        <LandingFooter />
       </div>
     </>
   );
--- src/component/swiper/Swiper.jsx
+++ src/component/swiper/Swiper.jsx
@@ -15,76 +15,94 @@ function Swipers() {
           className={styles.mySwiper}
           modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={10}
-          slidesPerView={3}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            780: {
+              slidesPerView: 3,
+            },
+            1024:{
+              slidesPerView:3
+            }
+          }}
           navigation={false}
           speed={200}
-          pagination={{clickable: true}}
+          pagination={{ clickable: true }}
           autoplay={true}
           loop={true}
         >
-          <SwiperSlide className={styles.span} style = {{backgroundColor: 'aliceblue'}}>
-            
-              <em>1/4</em>
-              <h2 style={{color:'green'}}>Culture: Embrace Diversity</h2>
-              <p>
-                <b style={{color:'green'}}>Interactive Forums</b>: Engage in Discussions and share
-                ideas. <br />
-                <b style={{color:'green'}}>Cultural Events</b>: Participate in virtual festivals,
-                exhibitions, and workshops. <br />
-                <b style={{color:'green'}}>Exclusive Content</b>: Access articles, videos, and podcasts
-                from cultural experts.
-              </p>
-            </SwiperSlide>
-           
-            <SwiperSlide className={styles.span} style = {{backgroundColor: 'aliceblue'}}>
-           
-              <em>2/4</em>
-              <h2 style={{color:'green'}}>
-                <b>Lifestyle</b>: <br />
-                Live Your Best Life.
-              </h2>
-              <p>
-                <b style={{color:'green'}}>Wellness Programs</b>: Find Fitness routines, mental health
-                tips, and nutrition advice. <br />
-                <b style={{color:'green'}}>Style Guides</b>: Stay updated with trends in fashion, beauty
-                and food. <br />
-                <b style={{color:'green'}}>Hobbies & Interests</b>: Explore new passions and connect
-                with like-minded individuals.
-              </p>
-            </SwiperSlide>
-           
-            <SwiperSlide className={styles.span} style = {{backgroundColor: 'aliceblue'}}>
-           
-              <em>3/4</em>
-              <h2 style={{color:'green'}}>
-                Social: <br />
-                Connect and Grow
-              </h2>
-              <p>
-                <b style={{color:'green'}}>Network</b>: Connect with members, sharing your interests and
-                professional goals. <br />
-                <b style={{color:'green'}}>Communities</b>: Join groups based on your hobbies,
-                professions or personal interests. <br />
-                <b style={{color:'green'}}>Events</b>: Attend online meetups, webinars and social
-                gatherings.
-              </p>
-           </SwiperSlide>
+          <SwiperSlide
+            className={styles.span}
+            style={{ backgroundColor: "aliceblue" }}
+          >
+            <em>1/4</em>
+            <h2 style={{ color: "green" }}>Culture: Embrace Diversity</h2>
+            <p>
+              <b style={{ color: "green" }}>Interactive Forums</b>: Engage in
+              Discussions and share ideas. <br />
+              <b style={{ color: "green" }}>Cultural Events</b>: Participate in
+              virtual festivals, exhibitions, and workshops. <br />
+              <b style={{ color: "green" }}>Exclusive Content</b>: Access
+              articles, videos, and podcasts from cultural experts.
+            </p>
+          </SwiperSlide>
 
-            <SwiperSlide className={styles.span} style = {{backgroundColor: 'aliceblue'}}>
-          
-              <em>4/4</em>
-              <h2 style={{color:'green'}}>
-                Commerce: <br />
-                Shop and Succeed
-              </h2>
-              <p>
-                <b style={{color:'green'}}>Marketplace</b>: Discover unique products from artisans and
-                small businesses. <br />
-                <b style={{color:'green'}}>Digital Banking</b>: Transform your finances and maximize
-                your {"money's"} potential. <br />
-                <b style={{color:'green'}}>Exclusive Deals</b>: Attend online meetups, webinars and
-                social gatherings.
-              </p>
+          <SwiperSlide
+            className={styles.span}
+            style={{ backgroundColor: "aliceblue" }}
+          >
+            <em>2/4</em>
+            <h2 style={{ color: "green" }}>
+              <b>Lifestyle</b>: <br />
+              Live Your Best Life.
+            </h2>
+            <p>
+              <b style={{ color: "green" }}>Wellness Programs</b>: Find Fitness
+              routines, mental health tips, and nutrition advice. <br />
+              <b style={{ color: "green" }}>Style Guides</b>: Stay updated with
+              trends in fashion, beauty and food. <br />
+              <b style={{ color: "green" }}>Hobbies & Interests</b>: Explore new
+              passions and connect with like-minded individuals.
+            </p>
+          </SwiperSlide>
+
+          <SwiperSlide
+            className={styles.span}
+            style={{ backgroundColor: "aliceblue" }}
+          >
+            <em>3/4</em>
+            <h2 style={{ color: "green" }}>
+              Social: <br />
+              Connect and Grow
+            </h2>
+            <p>
+              <b style={{ color: "green" }}>Network</b>: Connect with members,
+              sharing your interests and professional goals. <br />
+              <b style={{ color: "green" }}>Communities</b>: Join groups based
+              on your hobbies, professions or personal interests. <br />
+              <b style={{ color: "green" }}>Events</b>: Attend online meetups,
+              webinars and social gatherings.
+            </p>
+          </SwiperSlide>
+
+          <SwiperSlide
+            className={styles.span}
+            style={{ backgroundColor: "aliceblue" }}
+          >
+            <em>4/4</em>
+            <h2 style={{ color: "green" }}>
+              Commerce: <br />
+              Shop and Succeed
+            </h2>
+            <p>
+              <b style={{ color: "green" }}>Marketplace</b>: Discover unique
+              products from artisans and small businesses. <br />
+              <b style={{ color: "green" }}>Digital Banking</b>: Transform your
+              finances and maximize your {"money's"} potential. <br />
+              <b style={{ color: "green" }}>Exclusive Deals</b>: Attend online
+              meetups, webinars and social gatherings.
+            </p>
           </SwiperSlide>
         </Swiper>
       </div>
